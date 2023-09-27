8c1f62173e65e0b2350059eb1d13ac69a5ea1841
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-09-28 01:40:01 +0300

    style(other): Added styles contants

---
+++ src/style/color.ts
@@ -0,0 +1,10 @@
+export default {
+  ALABASTER: '#FBFBFB',
+  MONZA: '#DB040E',
+  MERCURY: '#E1E1E1',
+  SMOKE: '#BDBDBD',
+  BLACK: '#001C30',
+  NAVY: '#176B87',
+  TEAL: '#64CCC5',
+  MINT: '#DAFFFB',
+};
---
+++ src/style/font.ts
@@ -0,0 +1,7 @@
+export default {
+  BOLD: 'Quicksand-Bold',
+  LIGHT: 'Quicksand-Light',
+  MEDIUM: 'Quicksand-Medium',
+  REGULAR: 'Quicksand-Regular',
+  SEMIBOLD: 'Quicksand-SemiBold',
+};

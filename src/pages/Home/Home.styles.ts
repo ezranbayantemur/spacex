5a3764df5e63d5dbea8d46dc754431d4af35d6bb
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-09-28 01:42:23 +0300

    feat(pages): Added Home page structure

---
+++ src/pages/Home/Home.styles.ts
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+import {font, padding} from '@style';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    height: '48%',
+    width: '100%',
+    alignSelf: 'center',
+    tintColor: 'black',
+  },
+  title_container: {
+    flex: 1,
+    backgroundColor: 'black',
+    paddingTop: padding.MEDIUM,
+  },
+  header_title_text: {
+    fontSize: 50,
+    fontFamily: font.LIGHT,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  content_text: {
+    fontSize: 20,
+    fontFamily: font.REGULAR,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  footer: {
+    flex: 0,
+    backgroundColor: 'black',
+  },
+});
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,31 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View, Text, Image} from 'react-native';
+import {DateRangeSelector} from '@components';
+import styles from './Home.styles';
+
+const BACKGROUND_IMAGE = require('../../assets/rocket.png');
 
 const Home = () => {
+  const handleSearch = () => {};
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            resizeMode="contain"
+            source={BACKGROUND_IMAGE}
+          />
+          <View style={styles.title_container}>
+            <Text style={styles.header_title_text}>SPACEX FLIGHTS</Text>
+            <Text style={styles.content_text}>Search for flights</Text>
+
+            <DateRangeSelector onSearch={console.log} />
+          </View>
+        </View>
+      </SafeAreaView>
+      <SafeAreaView style={styles.footer} />
+    </>
   );
 };
 

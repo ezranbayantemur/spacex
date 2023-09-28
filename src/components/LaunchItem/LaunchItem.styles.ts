814f5c731ce5511bae4ab7a2eb58810447a72801
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-09-29 02:11:53 +0300

    fix(components): Fixed LaunchItem overflow bug

--- src/components/LaunchItem/LaunchItem.styles.ts
+++ src/components/LaunchItem/LaunchItem.styles.ts
@@ -1,19 +1,22 @@
 import {StyleSheet} from 'react-native';
-import {color, font, padding, typography} from '@style';
+import {font, padding, typography} from '@style';
 
 export default StyleSheet.create({
   container: {
-    margin: padding.MEDIUM,
-    padding: padding.MEDIUM,
+    margin: padding.SMALL,
+    padding: padding.SMALL,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
   image: {
+    marginRight: padding.SMALL,
     width: 50,
     height: 50,
   },
   name: {
+    flex: 1,
+    alignSelf: 'center',
     fontFamily: font.BOLD,
     fontSize: typography.BIG,
   },
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Button from './Button';
 import DatePicker from './DatePicker';
+import LaunchItem from './LaunchItem';
 import DateRangeSelector from './DateRangeSelector';
 
-export {Button, DateRangeSelector, DatePicker};
+export {Button, DateRangeSelector, DatePicker, LaunchItem};

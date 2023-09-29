8ccc3616591b0caa194afbac90657e7cc0b62f85
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-09-29 03:01:46 +0300

    chore(components): Updated index on components directory

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 import Input from './Input';
 import Button from './Button';
+import SearchBar from './SearchBar';
 import DatePicker from './DatePicker';
 import LaunchItem from './LaunchItem';
 import DateRangeSelector from './DateRangeSelector';
 
-export {Button, Input, DateRangeSelector, DatePicker, LaunchItem};
+export {Button, Input, SearchBar, DateRangeSelector, DatePicker, LaunchItem};
